bf7f2f09293ee48217bfb8b072f2482517967260
Miguel_Fr3 <rm99997@fiap.com.br>
2023-09-18 18:15:26 -0300

    finalizando html para add produto

--- src/routes/produtos/adicionar/AdicionarProdutos.jsx
+++ src/routes/produtos/adicionar/AdicionarProdutos.jsx
@@ -9,8 +9,23 @@ export default function AdicionarProdutos(){
             <h1></h1>
             <form >
                 <fieldset>
-                    <label htmlFor="idNome">Nome:</label>
-                    <input type="text" name="nome" id="idNome" />
+                    <div>
+                        <label htmlFor="idNome">Nome:</label>
+                        <input type="text" name="nome" id="idNome" />
+                    </div>
+                    <div>
+                        <label htmlFor="idDesc">Descrição:</label>
+                        <input type="text" name="desc" id="idDesc" />
+                    </div>
+                    <div>
+                        <label htmlFor="idPreco">Preço:</label>
+                        <input type="number" min="0" name="preco" id="idPreco" />
+                    </div>
+                    <div>
+                        <label htmlFor="idImg">Imagem:</label>
+                        <input type="text" name="Img" id="idImg" />
+                    </div>
+                    <button type="submit">Add</button>
                 </fieldset>
             </form>
         </main>
