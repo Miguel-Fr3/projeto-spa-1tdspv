139ab6cc016927ca9c4d7223e887a6b6f6ec1bba
Miguel_Fr3 <rm99997@fiap.com.br>
2023-09-18 18:11:58 -0300

    criando campo de nome no form

--- src/routes/produtos/adicionar/AdicionarProdutos.jsx
+++ src/routes/produtos/adicionar/AdicionarProdutos.jsx
@@ -2,5 +2,17 @@ import { useNavigate } from "react-router-dom";
 import { ListaProdutos } from "../../../components/ListaProdutos";
 
 export default function AdicionarProdutos(){
-    
+    const navigate = useNavigate();
+
+    return(
+        <main>
+            <h1></h1>
+            <form >
+                <fieldset>
+                    <label htmlFor="idNome">Nome:</label>
+                    <input type="text" name="nome" id="idNome" />
+                </fieldset>
+            </form>
+        </main>
+    )
 }
