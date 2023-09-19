b032f739e5b31a6c4a8442a6a2a8d80db2684af8
patrickjaguski <patrick_jaguski@hotmail.com>
2023-09-18 22:28:39 -0300

    Inserindo as funções no html

--- src/routes/produtos/adicionar/AdicionarProdutos.jsx
+++ src/routes/produtos/adicionar/AdicionarProdutos.jsx
@@ -1,12 +1,12 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { ListaProdutos } from "../../../components/ListaProdutos";
+import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
 
 export default function AdicionarProdutos (){
 
     const navegacao = useNavigate();
-		 
-		function criarId(){
+        
+    function criarId(){
         let maiorId = 0;
         ListaProdutos.forEach(elemento=>{
             if (elemento.id > maiorId){
@@ -17,8 +17,7 @@ export default function AdicionarProdutos (){
     }   
     const novoId = criarId();
 
-
-		const [produto, setProduto] = useState({
+    const [produto, setProduto] = useState({
         id: novoId,
         nome: "",
         desc: "",
@@ -26,14 +25,13 @@ export default function AdicionarProdutos (){
         img: "https://picsum.photos/100/100"
     });
 
-		
-		const handleChange = (event) =>{
+
+    const handleChange = (event) =>{
         const{name,value} = event.target;
         setProduto({...produto, [name]:value})
     }
 
-		
-		const handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         ListaProdutos.push(produto);
 
@@ -44,37 +42,34 @@ export default function AdicionarProdutos (){
         return navegacao("/produtos")
     }
 
+    document.title = "Adicionar Produto";
 
-        document.title = "Adicionar Produto";
-
-	return (
-		<>
-				<div>
-            <h1>Adicionar Produto</h1>
+    return(
+        <>  
             <div>
-                <form>
-                    <fieldset>
-                            <legend>Informaçõees do Produto</legend>
-                            <input type="hidden" name="id"/>
-                        <div>
-                            <label htmlFor="idProduto">Nome do produto</label>
-                            <input type="text" name = "nome" id="idProd"/>
-                        </div>
-                        <div>
-                            <label htmlFor="idDesc">Descrição</label>
-                            <input type="text" name = "desc" id="idDesc"/>
-                        </div>
-                        <div>
-                            <label htmlFor="idPreco">Preço do produto</label>
-                            <input type="text" name = "preco" id="idPreco"/>
-                        </div>
-                        <button >Adicionar</button>
-                        <button>Cancelar</button>
-                    </fieldset>
-                </form>
+                <h1>Adicionar Produto</h1>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <fieldset>
+                                <legend>Informações do Produto</legend>
+                                <input type="hidden" name="id"/>
+                            <div>
+                                <label htmlFor="idProduto">Nome do produto</label>
+                                <input type="text" name = "nome" id="idProd"  onChange={handleChange}/>
+                            </div>
+                            <div>
+                                <label htmlFor="idDesc">Descrição</label>
+                                <input type="text" name = "desc" id="idDesc" onChange={handleChange}/>
+                            </div>
+                            <div>
+                                <label htmlFor="idPreco">Preço do produto</label>
+                                <input type="text" name = "preco" id="idPreco"  onChange={handleChange}/>
+                            </div>
+                            <button >Adicionar</button>
+                            <button onClick={handleCancel}>Cancelar</button>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
-        </div>
-		</>
-	)
-					
-}
+        </>
+);}
