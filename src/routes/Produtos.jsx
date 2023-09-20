f3aaa55f79d1055d48e83b80cd8e24e1948c5a86
Miguel_fr3 <rm99997@fiap.com.br>
2023-09-19 21:52:43 -0300

    Ajuste da tabela

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -67,7 +67,7 @@ export default function Produtos() {
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan="5" className={classes.tableDataStyle}>Total de Produtos: {ListaProdutos.length}</td>
+                <td colSpan="6" className={classes.tableDataStyle}>Total de Produtos: {ListaProdutos.length}</td>
               </tr>
               <tr>
                 <td colSpan ="6"><Link to={`/adicionar/produtos/`}>Adicionar Produto</Link></td>
